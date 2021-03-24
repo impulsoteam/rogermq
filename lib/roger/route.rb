b303d4093f7833a978ed440dde54899a61a7f4ff
André Cavallari <cavallari@live.com>
2021-03-23 21:37:30 -0300

    Removed unecessary option

--- lib/roger/route.rb
+++ lib/roger/route.rb
@@ -2,7 +2,7 @@ module Roger
   class Route
     attr_accessor :consumer, :exchange, :queue, :routing_key, :consumer_key
 
-    def initialize(consumer, exchange, queue, routing_key = nil, route_options = {  })
+    def initialize(consumer, exchange, queue, routing_key = nil)
       @consumer = consumer
       @exchange = exchange
       @queue = queue
