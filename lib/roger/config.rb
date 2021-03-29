bb8d5771e34583d9d2a8fcfc66b8cf7ca93c97fe
André Cavallari <cavallari@live.com>
2021-03-29 20:18:07 -0300

    Add client_uri to config

--- lib/roger/config.rb
+++ lib/roger/config.rb
@@ -10,6 +10,10 @@ module Roger
       def log_level=(log_level)
         @log_level = log_level
       end
+
+      def client_uri
+        @client_uri ||= ENV['CLOUDAMQP_URL']
+      end
     end
   end
 end
