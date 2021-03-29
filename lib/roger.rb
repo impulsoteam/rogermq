91c40a31710c3242a6aeaca92c8ee28452d6d245
André Cavallari <cavallari@live.com>
2021-03-29 20:17:47 -0300

    Rename and reload setup class to server

--- lib/roger.rb
+++ lib/roger.rb
@@ -4,13 +4,28 @@ module Roger
   autoload :Config, 'roger/config'
   autoload :Logging, 'roger/logging'
   autoload :Route, 'roger/route'
-  autoload :Consumer, 'roger/consumer'
   autoload :Payload, 'roger/payload'
-  autoload :Setup, 'roger/setup'
+  autoload :Server, 'roger/server'
+  autoload :Consumer, 'roger/consumer'
+  autoload :Rpc, 'roger/rpc'
+  autoload :RpcConsumer, 'roger/rpc_consumer'
+  autoload :RpcRoute, 'roger/rpc_route'
 
   class << self
     def routes
       @routes ||= {}
+    end
+
+    def rpc_routes
+      @rpc_routes ||= {}
+    end
+
+    def rpc_queue
+      @rpc_queue ||= channel.queue('roger.rpc', exclusive: true)
+    end
+
+    def rpc_exchange
+      @rpc_exchange ||= channel.exchange('roger.rpc', type: :direct, exclusive: true)
     end
 
     def exchanges
@@ -22,15 +37,15 @@ module Roger
     end
 
     def broker
-      @broker ||= Bunny.new(ENV['CLOUDAMQP_URL'])
+      @broker ||= Bunny.new(Config.client_uri)
     end
 
     def channel
-      @channel ||= @broker.create_channel
+      @channel ||= broker.create_channel
     end
 
     def load_consumers
-      klasses = ::Rails.root.join('app/consumers').glob('**/*.rb')
+      klasses = ::Rails.root.join('app/roger/consumers').glob('**/*.rb')
       klasses.each { |klass| klass.basename(klass.extname.to_s).to_s.camelize.constantize }
     end
   end
