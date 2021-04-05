c81c47c8749d255c8a201dafae647e08a264861e
André Cavallari <cavallari@live.com>
2021-04-05 15:59:09 -0300

    Update configuration file

--- lib/roger/config.rb
+++ lib/roger/config.rb
@@ -1,22 +1,38 @@
+
+require 'optparse'
 require 'logger'
+require 'dotenv/load'
 
 module Roger
   class Config
+    @log_level ||= Logger::INFO
+    @client_uri ||= 'amqp://guest:guest@localhost:5672/'
+    @rpc_route_name ||= 'remote_procedure_calls'
+    @consumers_directory ||= 'consumers/**/*.rb'
+
     class << self
-      def log_level
-        @log_level ||= Logger::INFO
+      attr_accessor :client_uri, :rpc_route_name, :log_level, :consumers_directory
+
+      def parse!
+        parser = OptionParser.new do |opts|
+          opts.on('-C', '--config PATH', 'Load config file') do |v|
+            require_relative File.join(Dir.pwd, v)
+          end
+
+          opts.on('-r', '--load-rails', 'Load rails (only when rails is present)') do |v|
+            load_rails!
+          end
+        end
+
+        parser.parse!
       end
 
-      def log_level=(log_level)
-        @log_level = log_level
-      end
-
-      def client_uri
-        @client_uri ||= ENV['CLOUDAMQP_URL']
-      end
-
-      def rpc_route_name
-        @rpc_route_name ||= 'roger.remote_procedure_calls'
+      def load_rails!
+        rails_path = File.expand_path(File.join('.', 'config', 'environment.rb'))
+        raise Interrupt unless File.exist?(rails_path)
+        ENV['RACK_ENV'] ||= ENV['RAILS_ENV'] || 'development'
+        require rails_path
+        ::Rails.application.eager_load!
       end
     end
   end
