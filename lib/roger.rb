27bd6240a7bfc1e41fe3fa3c99df3969832246a5
André Cavallari <cavallari@live.com>
2021-04-08 16:15:46 -0300

    Move codes from main file to class

--- lib/roger.rb
+++ lib/roger.rb
@@ -1,50 +1,14 @@
 require 'bunny'
+require 'securerandom'
+require 'json'
+require 'active_support/core_ext/string'
+require 'active_support/concern'
 
 module Roger
+  autoload :App, 'roger/app'
+  autoload :Consumer, 'roger/consumer'
   autoload :Config, 'roger/config'
   autoload :Logging, 'roger/logging'
-  autoload :Route, 'roger/route'
-  autoload :Payload, 'roger/payload'
-  autoload :Server, 'roger/server'
-  autoload :Consumer, 'roger/consumer'
+  autoload :MessageProcessor, 'roger/message_processor'
   autoload :Rpc, 'roger/rpc'
-  autoload :RpcRoute, 'roger/rpc_route'
-
-  class << self
-    def routes
-      @routes ||= {}
-    end
-
-    def rpc_routes
-      @rpc_routes ||= {}
-    end
-
-    def rpc_queue
-      @rpc_queue ||= channel.queue(Config.rpc_route_name, exclusive: true)
-    end
-
-    def rpc_exchange
-      @rpc_exchange ||= channel.exchange(Config.rpc_route_name, type: :direct, auto_delete: true)
-    end
-
-    def exchanges
-      @exchanges ||= {}
-    end
-
-    def queues
-      @queues ||= {}
-    end
-
-    def broker
-      @broker ||= Bunny.new(Config.client_uri)
-    end
-
-    def channel
-      @channel ||= broker.create_channel
-    end
-
-    def load_consumers
-      Dir[Config.consumers_directory].each {  |path| require File.join(Dir.pwd, path) }
-    end
-  end
 end
