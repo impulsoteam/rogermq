03a99e6e495e1e0109143e4cab8a961593f501f2
André Cavallari <cavallari@live.com>
2021-04-08 16:22:56 -0300

    Refact consumer class

--- lib/roger/consumer.rb
+++ lib/roger/consumer.rb
@@ -1,27 +1,41 @@
 module Roger
   module Consumer
-    include Roger::Logging
+    extend ActiveSupport::Concern
 
-    attr_reader :body, :properties, :delivery_info
+    included do
+      include Roger::Logging
+      attr_reader :body, :properties, :delivery_info
 
-    def initialize(payload)
-      @body = payload.body
-      @properties = payload.properties
-      @delivery_info = payload.delivery_info
+      def initialize(delivery_info, properties, body = nil)
+        @delivery_info = delivery_info
+        @properties = properties
+        @body = body
+      end
     end
 
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
+    class_methods do
+      include Roger::Logging
 
-    module ClassMethods
-      def route(exchange, queue_name, routing_key = nil)
-        consumer_key = [exchange, queue_name, routing_key].reject { |c| c.to_s.strip.empty? }.join('.')
-        Roger.routes[consumer_key] ||= Route.new(ancestors.first, exchange, queue_name, routing_key)
+      def call(method, info, properties, body)
+        return new(info, properties).send(method, *body) if info[:exchange] == Config.rpc_channel
+        new(info, properties, body).send(method)
       end
 
-      def rpc(routing_key)
-        Roger.rpc_routes[routing_key] ||= RpcRoute.new(ancestors.first, routing_key)
+      def rpc(routing_key, method = :process)
+        bind(Config.rpc_channel, Config.rpc_channel, routing_key: routing_key, method: method)
+      end
+
+      def bind(exchange_name, queue_name, bind_options = {})
+        consumer_key = [exchange_name, queue_name, bind_options[:routing_key]].compact.join('.')
+        method = bind_options.delete(:method) || :process
+
+        App.consumers[consumer_key] ||= {
+          klass: ancestors.first,
+          queue_name: queue_name,
+          exchange_name: exchange_name,
+          bind_options: bind_options,
+          method: method
+        }
       end
     end
   end
